a94b42b569e42b0ca0158436d7f9080b27960931
Marcel267 <martinez.otano2000@web.de>
2024-03-23 23:25:57 +0100

    Add utilities.php

--- src/Controller.php
+++ src/Controller.php
@@ -8,6 +8,14 @@ class Controller
     {
         extract($data);
 
-        include "View/$view.php";
+        $file = APPROOT . '/src/View/' . $view . '.php';
+
+        // Check for view file
+        if (is_readable($file)) {
+            include $file;
+        } else {
+            // View does not exist
+            die('<h1> 404 Page not found </h1>');
+        }
     }
 }
