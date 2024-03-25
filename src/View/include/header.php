7e6ee25c46994c7479f1fbccfe770666c52c6889
Marcel267 <martinez.otano2000@web.de>
2024-03-25 21:28:59 +0100

    Add picocss

--- src/View/include/header.php
+++ src/View/include/header.php
@@ -5,11 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
     <title> <?= APPTITEL; ?> </title>
 </head>
 
-<body>
+<body class="container">
     <nav>
+        <ul>
+            <li><strong>Simple MVC</strong></li>
+        </ul>
         <ul>
             <li><a href="<?= URLROOT; ?>"> Home </a></li>
             <li><a href="<?= URLROOT; ?>/about"> About </a></li>
